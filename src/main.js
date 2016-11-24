c9769e247e3cf4f6049aca9a67de73ee281bb9c5
strothj <jason@jasons.io>
2016-11-24 06:41:02 -0500

    Added VueRouter and VueMaterial dependencies

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
 import App from './App';
+import createStore from './store';
 
 Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.use(VueMaterial);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: createStore(),
   render: h => h(App),
 });

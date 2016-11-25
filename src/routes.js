13b9fc9ebbae9e56f862a256f6cf9204136b4f76
strothj <jason@jasons.io>
2016-11-25 17:00:15 -0500

    Started login page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import VueRouter from 'vue-router';
 import IndexPage from 'components/IndexPage';
+import LoginPage from 'components/LoginPage';
 
 const routes = [
   { path: '/', component: IndexPage },
+  { path: '/login', component: LoginPage },
 ];
 
 const routerFactory = () => new VueRouter({

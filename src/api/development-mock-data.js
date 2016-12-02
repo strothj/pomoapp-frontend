dde41f58c021cc74e13f68341bdce5769b46b3ef
strothj <jason@jasons.io>
2016-12-02 07:00:20 -0500

    Reworking mockRequestWrapper

--- src/api/development-mock-data.js
+++ src/api/development-mock-data.js
@@ -1,29 +1,43 @@
 /* eslint-disable import/prefer-default-export */
-let testAuthRequestWrapper;
+let mockRequestWrapperFunc;
 
-// Wraps the request method used by the OAuth2 client. It replaces responses
-// with mock data.
-export function mockRequestWrapper(authClient) {
+async function handleTokenRequest() {
+  let response = {};
+  response = {
+    status: 400,
+    body: {
+      access_token: '123',
+      token_type: 'bearer',
+      expires_in: 3600,
+      refresh_token: '456',
+    },
+  };
+  return response;
+}
+
+// Wraps the request method used by the OAuth2 client. It returns mock data
+// instead of making a real web request.
+export async function mockRequestWrapper(authClient) {
   if (process.env.NODE_ENV !== 'development') return;
-  if (authClient.request === testAuthRequestWrapper) return;
+  if (authClient.request === mockRequestWrapperFunc) return;
 
-  testAuthRequestWrapper = (method, url, body, headers) => {
-    /* eslint-disable no-console */
-    console.log(`method: ${method}
-url: ${url}
-body: ${body}
-headers: ${headers}`);
-    /* eslint-enable no-console */
+  mockRequestWrapperFunc = (method, url, body, headers) => {
+    console.log(`method: ${method}\nurl: ${url}\nbody: ${body}\nheaders: ${headers}`); // eslint-disable-line no-console
 
-    return Promise.resolve({
-      status: 200,
-      body: {
-        access_token: '123',
-        token_type: 'bearer',
-        expires_in: 3600,
-        refresh_token: '456',
-      },
+    const request = { method, url, body, headers };
+    let response;
+
+    switch (url) {
+      case 'https://pomoapp.site/api/v1/oauth/access_token': response = handleTokenRequest(request); break;
+      default: response = handleTokenRequest(); break;
+    }
+
+    // Return api response with added latency.
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(response);
+      }, 100);
     });
   };
-  authClient.request = testAuthRequestWrapper; // eslint-disable-line no-param-reassign
+  authClient.request = mockRequestWrapperFunc; // eslint-disable-line no-param-reassign
 }

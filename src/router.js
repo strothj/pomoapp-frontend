2bab04d51c14f5cc4a57a08c08f616b4ef0cade9
strothj <jason@jasons.io>
2016-12-16 00:51:08 -0500

    Started integrating Auth0 authentication

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import VueRouter from 'vue-router';
 import IndexView from 'components/IndexView';
 import LayeredLayout from 'components/LayeredLayout';
-import LoginView from 'components/LoginView';
+// import LoginView from 'components/LoginView';
 
 const routes = [
   { path: '/', name: 'Root', component: IndexView },
   { path: '/projects', name: 'ProjectsView', component: LayeredLayout },
   { path: '/projects/:projectId', name: 'TasksView', component: LayeredLayout },
   { path: '/projects/:projectId/:taskId', name: 'PomodoroView', component: LayeredLayout },
-  { path: '/login', name: 'LoginView', component: LoginView },
+  // { path: '/login', name: 'LoginView', component: LoginView },
 ];
 
 const routerFactory = () => new VueRouter({
